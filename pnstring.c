c022d086f8d4cdc515f6df60f5377d0233f17afd
Ning <ningpeng@users.noreply.github.com>
2018-02-01 15:02:05 +0800

    Update pnstring.c
    add strnstr

--- pnstring.c
+++ pnstring.c
@@ -67,3 +67,28 @@ strlcpy(char *dst, const char *src, size_t siz)
         return(s - src - 1);    /* count does not include NUL */
 }
 
+/*
+ * from https://github.com/freebsd/freebsd/blob/master/lib/libc/string/strnstr.c
+ * Find the first occurrence of find in s, where the search is limited to the
+ * first slen characters of s.
+ */
+char *
+strnstr(const char *s, const char *find, size_t slen)
+{
+	char c, sc;
+	size_t len;
+
+	if ((c = *find++) != '\0') {
+		len = strlen(find);
+		do {
+			do {
+				if (slen-- < 1 || (sc = *s++) == '\0')
+					return (NULL);
+			} while (sc != c);
+			if (len > slen)
+				return (NULL);
+		} while (strncmp(s, find, len) != 0);
+		s--;
+	}
+	return ((char *)s);
+}
